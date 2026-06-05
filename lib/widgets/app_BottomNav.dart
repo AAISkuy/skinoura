b2baec35717c3c4fb15041ed11effbe08a0a810a
AAISkuy <ahmadfizalal21@gmail.com>
2026-06-05 08:32:56 +0700

    update nyicil navbar

--- lib/widgets/app_BottomNav.dart
+++ lib/widgets/app_BottomNav.dart
@@ -34,52 +34,51 @@ class _NavigasiDrawerState extends State<NavigasiDrawer> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color.fromARGB(255, 226, 237, 243),
-      body: SafeArea(
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-              child: Stack(
-                alignment: Alignment.center,
-                children: [
-                  const Center(
-                    child: Text(
-                      "Skinoura",
-                      style: TextStyle(
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                        color: Color(0xFF7C9A92),
-                      ),
-                    ),
-                  ),
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: InkWell(
-                      onTap: () {
-                        // Navigator.push(
-                        //   context,
-                        //   MaterialPageRoute(
-                        //     builder: (context) => const ProfilePage(),
-                        //   ),
-                        // );
-                      },
-                      child: CircleAvatar(
-                        radius: 22,
-                        backgroundImage: AssetImage(
-                          'assets/images/Exampprofil.jpeg',
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      body: _pages[_selectedIndex],
 
-            Expanded(child: _pages[_selectedIndex]),
-          ],
-        ),
-      ),
-
+      // SafeArea(
+      //   child: Column(
+      //     children: [
+      //       Padding(
+      //         padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+      //         child: Stack(
+      //           alignment: Alignment.center,
+      //           children: [
+      //             const Center(
+      //               child: Text(
+      //                 "Skinoura",
+      //                 style: TextStyle(
+      //                   fontSize: 24,
+      //                   fontWeight: FontWeight.bold,
+      //                   color: Color(0xFF7C9A92),
+      //                 ),
+      //               ),
+      //             ),
+      //             Align(
+      //               alignment: Alignment.centerRight,
+      //               child: InkWell(
+      //                 onTap: () {
+      //                   // Navigator.push(
+      //                   //   context,
+      //                   //   MaterialPageRoute(
+      //                   //     builder: (context) => const ProfilePage(),
+      //                   //   ),
+      //                   // );
+      //                 },
+      //                 child: CircleAvatar(
+      //                   radius: 22,
+      //                   backgroundImage: AssetImage(
+      //                     'assets/images/Exampprofil.jpeg',
+      //                   ),
+      //                 ),
+      //               ),
+      //             ),
+      //           ],
+      //         ),
+      //       ),
+      //     ],
+      //   ),
+      // ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedIndex,
         backgroundColor: Colors.white,
